88c7d7321d2795fdaefc9a64fc2a56f8baa9f828
highlander08 <santosray62@gmail.com>
2020-12-04 22:55:50 -0300

    alobb

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,8 +2,8 @@ import React from 'react';
 import '../App.css';
 // import { Button } from './Button';
 import './HeroSection.css';
-// import { Link } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
+// import CardItem from './CardItem'
 function HeroSection() {
   return (
     <div className="hero-container">
@@ -11,41 +11,46 @@ function HeroSection() {
       <h1>O MELHOR PET SHOP</h1>
       <p>Qual o seu filhote ?</p>
       <div className="hero-btns">
-        <a
-          href="/Cat"
-          className="Cat"
+      {/* <CardItem
+              src="images/birdimg.jpg"
+              text="Saúde e Conforto para seu pássaro"
+              label="Bird"
+              path="/Bird"
+            /> */}
+        <Link className="Cat"
+          to="/Cat"
           // buttonStyle="btn--outline"
           // buttonSize="btn--large"
         >
           Gato
-        </a>
+        </Link>
 
-        <a
-          href="/Dog"
+        <Link
+          to="/Dog"
           className="Dog"
           // buttonStyle="btn--outline"
           // buttonSize="btn--large"
         >
           Cachorro
-        </a>
+        </Link>
 
-        <a
-          href="Bird"
+        <Link
+          to="/Bird"
           className="Bird"
           // buttonStyle="btn--outline"
           // buttonSize="btn--large"
         >
           Passaro
-        </a>
-        <a
-          href="products"
+        </Link>
+        <Link
+          to="/products"
           className="btns"
           // buttonStyle="btn--primary"
           // buttonSize="btn--large"
           onClick={console.log('hey')}
         >
           Melhor Preço da Região <i className="far fa-play-circle" />
-        </a>
+        </Link>
       </div>
     </div>
   );
